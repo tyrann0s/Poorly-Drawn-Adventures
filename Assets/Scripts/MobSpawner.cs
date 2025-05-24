d4c77856c8224ac536487c5f7db8c4ed1487750b
tyrann0s <work.tyran@gmail.com>
2025-05-24 12:09:00 +0700

    rider install

--- Assets/Scripts/MobSpawner.cs
+++ Assets/Scripts/MobSpawner.cs
@@ -1,16 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class MobSpawner : MonoBehaviour
 {
-    [SerializeField]
+    [SerializeField, Tooltip("If true, the spawned mob will be mirrored and hostile")]
     private bool isHostile;
 
-    [SerializeField]
-    private List<GameObject> mobPrefabs = new List<GameObject>();
+    [SerializeField, Tooltip("List of mob prefabs that can be spawned")]
+    private List<GameObject> mobPrefabs = new();
 
     private Mob currentMob;
+    private GameManager gameManager;
+
+    private void Awake()
+    {
+        gameManager = FindFirstObjectByType<GameManager>();
+        if (gameManager == null)
+        {
+            Debug.LogError($"GameManager not found in scene");
+        }
+    }
 
     private void Start()
     {
@@ -19,6 +28,7 @@ public class MobSpawner : MonoBehaviour
 
     public void SpawnMob()
     {
+        ClearCurrentMob();
         InstantiateMob();
 
         if (isHostile)
@@ -29,12 +39,39 @@ public class MobSpawner : MonoBehaviour
 
     private void InstantiateMob()
     {
-        int rand = Random.Range(0, mobPrefabs.Count);
-        GameObject mobGO = Instantiate(mobPrefabs[rand],  transform);
-        currentMob = mobGO.GetComponent<Mob>();
-        currentMob.IsHostile = isHostile;
-        currentMob.OriginPosition = new Vector3 (transform.position.x, transform.position.y + currentMob.transform.localPosition.y, transform.position.y);
+        if (mobPrefabs == null || mobPrefabs.Count == 0)
+        {
+            Debug.LogError($"No mob prefabs assigned to {gameObject.name}");
+            return;
+        }
 
-        FindObjectOfType<GameManager>().AddMob(currentMob);
+        int rand = Random.Range(0, mobPrefabs.Count);
+        GameObject mobGo = Instantiate(mobPrefabs[rand], transform);
+        
+        currentMob = mobGo.GetComponent<Mob>();
+        if (currentMob == null)
+        {
+            Debug.LogError($"Mob component not found on prefab {mobGo.name}");
+            Destroy(mobGo);
+            return;
+        }
+
+        currentMob.IsHostile = isHostile;
+        currentMob.OriginPosition = new Vector3(
+            transform.position.x,
+            transform.position.y + currentMob.transform.localPosition.y,
+            transform.position.z
+        );
+
+        gameManager.AddMob(currentMob);
+    }
+
+    public void ClearCurrentMob()
+    {
+        if (currentMob != null)
+        {
+            Destroy(currentMob.gameObject);
+            currentMob = null;
+        }
     }
 }

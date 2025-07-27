d46f5204fc0406a3f88a4dd9f3ed5560057a64d7
tyrann0s <work.tyran@gmail.com>
2025-07-27 08:57:33 +0700

    AOE Damage skill

---
+++ Assets/Scripts/Mobs/Skills/Active/AOEDamageSkill.cs
@@ -0,0 +1,25 @@
+using Managers;
+using Mobs;
+using UnityEngine;
+
+[ CreateAssetMenu (fileName = "AOE Damage", menuName = "Data/Skills/Active Skills/AOE Damage", order = 1)]
+public class AOEDamageSkill : ActiveSkill
+{
+    private void Reset()
+    {
+        SkillName = "AOE Damage";
+        
+        IsAttack = true;
+        IsRanged = true;
+    }
+    
+    public override void Use(Mob parentMob, Mob targetMob)
+    {
+        base.Use(parentMob, targetMob);
+
+        foreach (var enemyMob in ServiceLocator.Get<MobManager>().EnemyMobs)
+        {
+            enemyMob.MobCombatSystem.GetDamage(Amount, enemyMob.CurrentCombo);
+        }
+    }
+}
